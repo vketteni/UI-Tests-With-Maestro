50655bc33bc6acc1cd98bccb59ca0ba8dc3a2f96
vketteni <v.ketteniss@outlook.com>
2023-02-26 13:53:59 +0100

    rm emails helper from git

--- .helper/emails.js
+++
@@ -1,8 +0,0 @@
-output.emails = {
-"testui" : "vincent+testui@kptncook.com",
-"login" : "vincent+testui+login@kptncook.com",
-"registration" : "vincent+testui+registration@kptncook.com",
-"premium" : "vincent+testui+premium@kptncook.com",
-"free" : "vincent+testui+free@kptncook.com",
-"friend" : "vincent+testui+friend@kptncook.com"
-}
