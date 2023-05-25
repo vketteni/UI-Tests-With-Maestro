5cee7791627a027c5de1610a6122ed3850c67d64
vketteni <v.ketteniss@outlook.com>
2023-05-25 13:31:37 +0200

    *1.3.1

---
+++ .helper/emails.js
@@ -0,0 +1,37 @@
+// TODO: REFACTOR ALL HARDCODED EMAILS
+output.emails = {
+"testui" : {
+    "email" : "vincent+testui@kptncook.com",
+    "password" : "testing",
+},
+"login" : {
+    "email" : "vincent+testui+login@kptncook.com",
+    "password" : "testing",
+},
+"registration" : {
+    "email" : "vincent+testui+registration@kptncook.com",
+    "password" : "testing",
+},
+"premium" : {
+    "email" : "vincent+testui+premium@kptncook.com",
+    "password" : "testing",
+},
+"free" : {
+    "email" : "vincent+testui+free@kptncook.com",
+    "password" : "testing",
+},
+"settings" : {
+    "email" : "vincent+testui+settings@kptncook.com",
+    "password" : "testing",
+},
+"friend" : {
+    "email" : "vincent+testui+friend@kptncook.com",
+    "password" : "testing"
+},
+"accountDeletion" : {
+    "email" : "vincent+testui+deletion@kptncook.com",
+    "password" : "testing",
+    "firstname" : "AccountDeletion",
+    "lastname" : "Economy",
+},
+}
